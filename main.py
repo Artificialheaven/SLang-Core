b2454a66db2cd506bb9fd37a47ca38831c786f13
xiaoyao <3399752707@qq.com>
2024-07-30 21:58:40 +0800

    更新了main的逻辑

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import Core.init
-import Core.main
+from Core.init import init as slang_init
 from regs import register
 from Core.main import global_dict
 
 
 # 这个地方实际上会先运行基础的init，例如globals和reg的注册
-Slang = Core.init.init(register, d={'debugging': True})    # 这里进行初始化检测
+Slang = slang_init(register, d={'debugging': True})    # 这里进行初始化检测
 global_dict['debug'] = True
 
 try:
@@ -15,4 +14,4 @@ try:
 except KeyboardInterrupt as e:
     print("\nUser Exit Slang")
     exit()
-    
+
