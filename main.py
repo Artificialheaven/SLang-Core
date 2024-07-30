6bb60a462297791b06f29d08b53d39c2fd0963e2
Artificialheaven <3399752707@qq.com>
2024-07-30 21:49:43 +0800

    dev2

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import Core.init
-import Core.main
+from Core import init
+
 from regs import register
 from Core.main import global_dict
 
 
 # 这个地方实际上会先运行基础的init，例如globals和reg的注册
-Slang = Core.init.init(register, d={'debugging': True})    # 这里进行初始化检测
+Slang = init.init(register, d={'debugging': True})    # 这里进行初始化检测
 global_dict['debug'] = True
 
 try:
